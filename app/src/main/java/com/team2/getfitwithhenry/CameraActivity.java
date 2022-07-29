285f53f243aad05eb54044799e070876e6d28037
Anandeeswaran <e0941664@u.nus.edu>
2022-07-29 17:39:01 +0800

    camera to java send image as byte

--- app/src/main/java/com/team2/getfitwithhenry/CameraActivity.java
+++ app/src/main/java/com/team2/getfitwithhenry/CameraActivity.java
@@ -7,22 +7,18 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Path;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Looper;
-import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -30,14 +26,8 @@ import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
 import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -53,15 +43,14 @@ import okhttp3.ResponseBody;
 public class CameraActivity extends AppCompatActivity {
 
     private final OkHttpClient client = new OkHttpClient();
-
+    private static final MediaType MEDIA_TYPE_PLAINTEXT = MediaType
+            .parse("text/plain; charset=utf-8");
     private final int REQUEST_CODE_PERMISSIONS = 10;
-    private String[] REQUIRED_PERMISSIONS = new String[] {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
-
-    private final int CAPTURE_IMAGE_REQUEST = 1;
+    private final String[] REQUIRED_PERMISSIONS = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
     private File photoFile;
-    private String mCurrentPhotoPath;
     private ImageView mimageView;
     private TextView resultsText;
+    private Bitmap bitImage;
     ActivityResultLauncher<Intent> captureImageResult;
 
     @Override
@@ -74,29 +63,22 @@ public class CameraActivity extends AppCompatActivity {
 
         registerActivityResult();
 
-        if(havePermission()){
+        if (havePermission()) {
             startCameraAndWriteToFile();
-        }
-        else{
+        } else {
             requestPermission();
         }
 
 
-
     }
 
-    protected Boolean havePermission(){
-        if(ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.CAMERA)
-                == PackageManager.PERMISSION_GRANTED){
-            return true;
-        }
-        else{
-            return false;
-        }
+    protected Boolean havePermission() {
+        return ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED;
     }
 
     private void requestPermission() {
-        ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS,REQUEST_CODE_PERMISSIONS);
+        ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
     }
 
     @Override
@@ -119,6 +101,7 @@ public class CameraActivity extends AppCompatActivity {
             String[] permissions = new String[]{
                     Manifest.permission.WRITE_EXTERNAL_STORAGE
             };
+            int CAPTURE_IMAGE_REQUEST = 1;
             ActivityCompat.requestPermissions(this, permissions, CAPTURE_IMAGE_REQUEST);
         }
     }
@@ -152,8 +135,8 @@ public class CameraActivity extends AppCompatActivity {
         String timeStamp = DateFormat();
         String imgFileName = "JPEG_" + timeStamp + "_";
         File dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File myFile = new File(dir, imgFileName);
-        mCurrentPhotoPath = myFile.getAbsolutePath();
+        //File myFile = new File(dir, imgFileName);
+        //String mCurrentPhotoPath = myFile.getAbsolutePath();
         return (File.createTempFile(imgFileName, ".jpg", dir));
     }
 
@@ -168,7 +151,7 @@ public class CameraActivity extends AppCompatActivity {
                 new ActivityResultContracts.StartActivityForResult(), result -> {
                     if (result.getResultCode() == AppCompatActivity.RESULT_OK) {
                         Intent data = result.getData();
-                        Bitmap bitImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                        bitImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
                         mimageView.setImageBitmap(bitImage);
                         Toast.makeText(getBaseContext(), "Showing the image", Toast.LENGTH_LONG).show();
                         uploadRequestBody();
@@ -176,9 +159,17 @@ public class CameraActivity extends AppCompatActivity {
                 });
     }
 
+    public byte[] getBytesFromBitmap(Bitmap bitmap) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 70, stream);
+        return stream.toByteArray();
+    }
+
     private void uploadRequestBody() {
+
         Request request = new Request.Builder()
-                .url("http://192.168.10.127:8080/test/method1")
+                .url("http://192.168.0.111:8080/flask/recieveImgFromAndroid")
+                .post(RequestBody.create(MEDIA_TYPE_PLAINTEXT, getBytesFromBitmap(bitImage)))
                 .build();
 
         client.newCall(request).enqueue(new Callback() {
@@ -196,7 +187,8 @@ public class CameraActivity extends AppCompatActivity {
                     }
 
                     String msg = String.valueOf(responseBody.string());
-                    backgroundThreadShortToast(getApplicationContext(),msg);
+
+                    backgroundThreadShortToast(getApplicationContext(), msg);
 
                     Log.i("data", responseBody.string());
                 } catch (Exception e) {
@@ -205,18 +197,11 @@ public class CameraActivity extends AppCompatActivity {
             }
         });
 
-
     }
 
     public static void backgroundThreadShortToast(final Context context, final String msg) {
         if (context != null && msg != null) {
-            new Handler(Looper.getMainLooper()).post(new Runnable() {
-
-                @Override
-                public void run() {
-                    Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
-                }
-            });
+            new Handler(Looper.getMainLooper()).post(() -> Toast.makeText(context, msg, Toast.LENGTH_SHORT).show());
         }
     }
 
