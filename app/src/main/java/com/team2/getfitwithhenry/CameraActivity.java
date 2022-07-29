815a9bc3d8eaa1af3f931741023f2b1d03cb6ef4
alylim <lim.alywx@gmail.com>
2022-07-29 09:20:20 +0800

    adding test okHttp activity and dependencies to ping java server

--- app/src/main/java/com/team2/getfitwithhenry/CameraActivity.java
+++ app/src/main/java/com/team2/getfitwithhenry/CameraActivity.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Path;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -22,12 +25,30 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class CameraActivity extends AppCompatActivity {
+
+    //this is for okhttp testing
+    public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
 
     private final int CAPTURE_IMAGE_REQUEST = 1;
     private File photoFile;
@@ -47,12 +68,13 @@ public class CameraActivity extends AppCompatActivity {
 
         registerActivityResult();
 
+
     }
 
 
     private void captureImage() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-            startCameraandWritetoFile();
+            startCameraAndWriteToFile();
         } else {
             String[] permissions = new String[]{
                     Manifest.permission.WRITE_EXTERNAL_STORAGE
@@ -61,7 +83,7 @@ public class CameraActivity extends AppCompatActivity {
         }
     }
 
-    private void startCameraandWritetoFile() {
+    private void startCameraAndWriteToFile() {
         Intent captureImageIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
         if (captureImageIntent.resolveActivity(getPackageManager()) != null) {
@@ -105,11 +127,52 @@ public class CameraActivity extends AppCompatActivity {
                 new ActivityResultContracts.StartActivityForResult(), result -> {
                     if (result.getResultCode() == AppCompatActivity.RESULT_OK) {
                         Intent data = result.getData();
-
                         Bitmap bitImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
                         mimageView.setImageBitmap(bitImage);
                         Toast.makeText(getBaseContext(), "Showing the image", Toast.LENGTH_LONG).show();
+                        uploadRequestBody();
                     }
                 });
     }
-}
+
+    private void uploadRequestBody() {
+        System.out.println("inside method");
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("http://localhost:8080/test/method1")
+
+                .build();
+
+        new Thread(new Runnable(){
+            @Override
+            public void run(){
+                Log.i("In thread", "run");
+                try (Response response = client.newCall(request).execute()) {
+                    Log.i("Inside try", "try");
+                    if (!response.isSuccessful()) {
+                        Log.i("In response", "not success");
+                        throw new IOException("Unexpected code " + response);
+
+                    }
+
+                    Log.i("In thread", "received response");
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                    System.out.println("something went wrong");
+            }
+        }});
+
+
+
+    }
+
+    }
+
+
+
+
+
+
