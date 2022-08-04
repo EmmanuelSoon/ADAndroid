dd847afd49e3c26e64778f2e196872f46fd69d62
yoonmiemieaung <e0941690@u.nus.edu>
2022-08-04 11:35:47 +0800

    solved conflict

--- app/src/main/java/com/team2/getfitwithhenry/RecipeActivity.java
+++ app/src/main/java/com/team2/getfitwithhenry/RecipeActivity.java
@@ -3,14 +3,13 @@ package com.team2.getfitwithhenry;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-<<<<<<< HEAD
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class RecipeActivity extends AppCompatActivity {
 
     private WebView mWebView;
-    private String mUrl = "http://192.168.10.122:3000";
+    private String mUrl = "http://192.168.1.79:3000";
 //    private String mUrl = "http://localhost:3000";
 
     @Override
@@ -22,14 +21,6 @@ public class RecipeActivity extends AppCompatActivity {
         mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.setWebViewClient(new WebViewClient());
         mWebView.loadUrl(mUrl);
-=======
 
-public class RecipeActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_receipe);
->>>>>>> 8968fb2059a702085c947ae3a6d9b7e310df40c3
     }
 }
