e09072c4241243f5bfc7f8213b06bc4dd038f237
Emman <reach.emmanuelsoon@gmail.com>
2022-08-15 11:23:56 +0800

    bug fix (camera placeholder and negative water)

--- app/src/main/java/com/team2/getfitwithhenry/model/Constants.java
+++ app/src/main/java/com/team2/getfitwithhenry/model/Constants.java
@@ -1,6 +1,6 @@
 package com.team2.getfitwithhenry.model;
 
 public class Constants {
-    public static String javaURL = "http://159.223.77.157:8080";
+    public static String javaURL = "http://10.249.219.167:8080";
     public static String reactURL = "http://159.223.77.157:3000/android";
 }
