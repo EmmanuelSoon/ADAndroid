fa6d3ac8db98ccd2b5725555571f9d4a5e8264c3
Emmanuel Soon <reach.emmanuelsoon@gmail.com>
2022-08-16 19:49:24 +0800

    bug fix: webview photo upload

--- app/src/main/java/com/team2/getfitwithhenry/model/Constants.java
+++ app/src/main/java/com/team2/getfitwithhenry/model/Constants.java
@@ -4,4 +4,6 @@ public class Constants {
     public static String javaURL = "http://192.168.10.122:8080";
     public static String reactURL = "http://192.168.10.122:3000/android";
 
+//    public static String javaURL = "http://159.223.77.157:8080";
+//    public static String reactURL = "http://159.223.77.157:3000/android";
 }
